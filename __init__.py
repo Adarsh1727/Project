26cb0bfd7e5c108b5b4de2cfa39e4a8c4358e755
Adarsh singh <135491640+Adarsh1727@users.noreply.github.com>
2023-06-19 21:35:25 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,12 @@
-from telethon import TelegramClient 
-import telebot
+from telethon import TelegramClient
 
-import logging 
- 
-import time 
- 
-openai_key = "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"
- 
-api_id ="1125689"
-api_hash ="4772d1792ed194020a8fb06a91ffb8fa"
-bot_token ="5907130062:AAHtiF3fTjWSxG4EI-xE3w12Cx6LJp-_ovU"
- 
-bot = TelegramClient("rjbot",api_id,api_hash).start(bot_token=bot_token)
+import logging
+
+import time
+
+
+api_id = "25058178"
+api_hash = "131929c537e9d4914c045f0e01d5cda3"
+bot_token = "5965355150:AAGRxhv6Rd2HDqov9yDY-gtmIG31OwHmJxQ"
+
+bot = TelegramClient("aadi",api_id,api_hash).start(bot_token = bot_token)
